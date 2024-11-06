75946db005c42170855a23c619591fd6cc29fcfc
허철용 <cyheo@naver.com>
2024-11-06 22:46:42 +0900

    KYT

---
+++ src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Count: {{ counterStore.count }}</p>
+        <p>Double Count: {{ counterStore.doubleCount }}</p>
+        <button @clikc="counterStore.increment">Increment</button>
+        <button @clikc="counterStore.decrement">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import { useCounterStore } from '@stores/counter'
+
+    const counterState = useCounterStore();
+</script>
+<style>
+    
+</style>
