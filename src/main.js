91b91c328bc569a8874c9e81bb554e591bda1907
허철용 <cyheo@naver.com>
2024-11-07 20:52:26 +0900

    conflict resolve

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue'
@@ -8,9 +7,3 @@ const pinia = createPinia();
 
 app.use(pinia);
 app.mount('#app');
-=======
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
->>>>>>> parent of 50fe75b (pinia)
