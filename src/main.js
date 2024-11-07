4a491495a8715ea7e95af66114f0689a96f96963
허철용 <cyheo@naver.com>
2024-11-07 20:48:32 +0900

    dev to master version revert

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue'
@@ -7,3 +8,9 @@ const pinia = createPinia();
 
 app.use(pinia);
 app.mount('#app');
+=======
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+>>>>>>> parent of 50fe75b (pinia)
