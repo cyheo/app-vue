8df177f0c59d5b96658a47aefbc4bed06ef702f3
허철용 <cyheo@naver.com>
2024-11-07 21:01:42 +0900

    reset edf7dab

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
   <NaviBar />
   <MovieInfo :data="data" @increase="increase($event)"/>
+  <p>Count: {{ counterStore.count }}</p>
+  <p>Double Count: {{ counterStore.doubleCount }}</p>
+  <button @click="counterStore.increment">Increment</button>
+  <button @click="counterStore.decrement">Decrement</button>
 </template>
 
 <script>
-  import data from './assets/movies'
-  import MoviInfo from './components/MovieInfo.vue';
-  import NaviBar from './components/NaviBar.vue';
+  import data from '@/assets/movies'
+  import MoviInfo from '@/components/MovieInfo.vue';
+  import NaviBar from '@/components/NaviBar.vue';
+  import { useCounterStore } from '@/stores/counter';
 
   export default {
     name: 'App',
+    setup() {
+      const counterStore = useCounterStore();
+      return { counterStore };
+    },
     data() {
       return {
         isModal: false,
