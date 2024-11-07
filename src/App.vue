625ce28d1bda09d70be69a8299190b5c0f1f37d4
heo.cheolyong <heo.cheolyong@eland-partner.co.kr>
2024-11-08 08:06:36 +0900

    2024-11-08 vue study

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,18 @@
 <template>
-  <NaviBar />
-  <MovieInfo :data="data" @increase="increase($event)"/>
+  <h1>Hello World</h1>
 </template>
 
 <script>
-  import data from './assets/movies'
-  import MoviInfo from './components/MovieInfo.vue';
-  import NaviBar from './components/NaviBar.vue';
-
   export default {
     name: 'App',
     data() {
       return {
-        isModal: false,
-        data: data,
       }
     },
     methods: {
-      increase(index) {
-        this.data[index].count += 1;
-      }
     },
     components : {
-      NaviBar: NaviBar,
-      MovieInfo: MoviInfo,
     }
 
   }
 </script>
-
-<style>
-  .bg-yellow {
-    background: gold;
-    padding: 10px;
-  }
-</style>
