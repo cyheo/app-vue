86932702d17049757314c3c3aebadd4a32c59ed9
heo.cheolyong <heo.cheolyong@eland-partner.co.kr>
2024-11-13 16:40:13 +0900

    ... Not Special Modify

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
-import axios from 'axios';
+//import axios from 'axios';
 
 const app = createApp(App);
 const pinia = createPinia();
