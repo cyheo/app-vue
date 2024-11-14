909919cff1184ba0dd8fbec250ae188b7e7d6f4d
허철용 <cyheo@naver.com>
2024-11-14 23:46:02 +0900

    axios example

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
-//import axios from 'axios';
+import axios from 'axios';
 
 const app = createApp(App);
 const pinia = createPinia();
+
+app.config.globalProperties.$axios = axios;
+
 app.use(pinia);
 app.use(router);
 app.mount('#app');
