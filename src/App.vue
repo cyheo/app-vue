e9057600e7687d80dad2521c87b3eba248e32eb2
heo.cheolyong <heo.cheolyong@eland-partner.co.kr>
2024-11-20 16:16:15 +0900

    conflict code modify

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <NaviBar />
-  <MovieInfo :data="data" @increase="increase($event)"/>
-  <p>Count: {{ counterStore.count }}</p>
-  <p>Double Count: {{ counterStore.doubleCount }}</p>
-  <button @click="counterStore.increment">Increment</button>
-  <button @click="counterStore.decrement">Decrement</button>
-</template>
-
-<script>
-  import data from '@/assets/movies'
-  import MoviInfo from '@/components/MovieInfo.vue';
-  import NaviBar from '@/components/NaviBar.vue';
-  import { useCounterStore } from '@/stores/counter';
-
-  export default {
-    name: 'App',
-    setup() {
-      const counterStore = useCounterStore();
-      return { counterStore };
-    },
-    data() {
-      return {
-        isModal: false,
-        data: data,
-      }
-    },
-    methods: {
-      increase(index) {
-        this.data[index].count += 1;
-      }
-    },
-    components : {
-      NaviBar: NaviBar,
-      MovieInfo: MoviInfo,
-=======
   <div id="app">
     <NavBar />
   </div>
@@ -47,7 +11,6 @@
     name: "App",
     components: {
       NavBar : NavBar
->>>>>>> dev_1
     }
   };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
 import { createApp } from 'vue';
-<<<<<<< HEAD
-import App from './App.vue'
-import { createPinia } from 'pinia';
-=======
 import { createPinia } from 'pinia';
 import router from './router';
 import App from './App.vue';
 import axios from 'axios';
->>>>>>> dev_1
 
 const app = createApp(App);
 const pinia = createPinia();
 
-<<<<<<< HEAD
-app.use( pinia );
-app.mount('#app');
-=======
 app.config.globalProperties.$axios = axios;
 
 app.use(pinia);
 app.use(router);
 app.mount('#app');
->>>>>>> dev_1
